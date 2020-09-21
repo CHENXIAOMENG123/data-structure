bf5c409c33004e7735b66515cb941a076f406f88
CHENXIAOMENG123 <49112145+CHENXIAOMENG123@users.noreply.github.com>
2020-09-21 15:44:05 +0800

    Add files via upload

---
+++ Find/bsearch_recursion.py
@@ -0,0 +1,17 @@
+from typing import List
+
+"""用递归实二分查找"""
+
+def bearch(nums:List[int],target:int)->int:
+    return bsearch_internally(nums,0,len(nums)-1,target)
+
+def bsearch_internally(nums:List[int],low:int,high:int,target:int):
+    if low>high:
+        return -1
+    mid=low+((high-low)>>2)
+    if nums[mid]==target:
+        return mid
+    elif nums[mid]<target:
+        return bsearch_internally(nums,mid+1,high,target) #从后半部分找
+    else:
+        return bsearch_internally(nums,low,mid-1,target)   #从前半部分找
